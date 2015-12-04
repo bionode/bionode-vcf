dc53ed1202fdb9e3f730b09633db74e770718d67
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2015-12-04 12:49:27 +0000

    initial parse and test written

--- test/test_vcf.js
+++ test/test_vcf.js
@@ -7,27 +7,67 @@
  */
 
 // chai is an assertion library
-var chai = require('chai');
+//var chai = require('chai');
 
 // @see http://chaijs.com/api/assert/
-var assert = chai.assert;
+//var assert = chai.assert;
 
 // register alternative styles
 // @see http://chaijs.com/api/bdd/
-chai.expect();
-chai.should();
+//chai.expect();
+//chai.should();
 
 // requires your main app (specified in index.js)
-var vcf = require('../');
+var VCF = require('../index');
+var path = require('path');
+var assert = require('assert');
+var filePath = path.join(__dirname, '../sample.vcf');
 
-describe('biojs-vcf module', function(){
-  describe('#hello()', function(){
-    it('should return a hello', function(){
+var allFeatures = [];
 
-      assert.equal(vcf.hello('biojs'), ("hello biojs"));
-      
-      // alternative styles
-      vcf.hello('biojs').should.equal("hello biojs");
+describe('VFF', function(){
+  describe('.read', function(){
+    it('should read without error', function(done){
+
+        function onFeature(vcf) {
+            allFeatures.push(vcf);
+        }
+
+        VCF.read(filePath).on('data', onFeature).on('end', done);
+
     });
+    it('should look like a valid output', function () {
+        assert.notStrictEqual(allFeatures, validOutput);
+    })
   });
 });
+
+
+var validOutput = [{
+    chr: 'Cf746836_TGAC_s1v1_scaffold_4',
+    pos: '5607',
+    id: '.',
+    ref: 'G',
+    alt: 'C',
+    qual: '18.1',
+    filter: '.',
+    varinfo: {
+        DP: '6',
+        VDB: '6.560000e-02',
+        RPB: '1.427508e+00',
+        AF1:' 0.5',
+        AC1: '1',
+        DP4: '3,1,1,1',
+        MQ: '60',
+        FQ: '21',
+        PV4: '1,0.0023,1,1'
+    },
+    sampleinfo: {
+        'foxley_wood1_bwa-mem-sorted.bam': {
+            GT: '0/1',
+            PL: '48,0,123',
+            GQ: '51'
+        }
+    }
+}
+];
