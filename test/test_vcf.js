c3aa7ae5dfad9425d866d22c4d037006613163de
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2015-12-07 15:59:46 +0000

    sample info produced as an array instead of object

--- test/test_vcf.js
+++ test/test_vcf.js
@@ -33,7 +33,7 @@ describe('VCF', function(){
             allFeatures.push(vcf);
         }
 
-        VCF.read(filePath).on('features', onFeature).on('end', done);
+        VCF.read(filePath).on('data', onFeature).on('end', done);
 
     });
     it('should look like a valid output', function () {
@@ -44,16 +44,15 @@ describe('VCF', function(){
 
 
 var validOutput = [
-    {
-        chr: 'Cf746836_TGAC_s1v1_scaffold_4',
+    {   chr: 'Cf746836_TGAC_s1v1_scaffold_4',
         pos: '5607',
         id: '.',
         ref: 'G',
         alt: 'C',
         qual: '18.1',
         filter: '.',
-        varinfo: {
-            VAR: 'SNP',
+        varinfo:
+        {   VAR: 'SNP',
             DP: '6',
             VDB: '6.560000e-02',
             RPB: '1.427508e+00',
@@ -62,32 +61,26 @@ var validOutput = [
             DP4: '3,1,1,1',
             MQ: '60',
             FQ: '21',
-            PV4: '1,0.0023,1,1'
-        },
-        sampleinfo: {
-            'foxley_wood1_bwa-mem-sorted.bam': {
+            PV4: '1,0.0023,1,1' },
+        sampleinfo:
+            [ { NAME: 'foxley_wood1_bwa-mem-sorted.bam',
                 GT: '0/1',
                 PL: '48,0,123',
-                GQ: '51'
-            }
-        },
-        attributes: {
-            vcfver: 'VCFv4.1',
+                GQ: '51' } ],
+        attributes:
+        {   vcfver: 'VCFv4.1',
             samtools: '0.1.19-44428cd',
-            reference: 'file://../index/Chalara_fraxinea_TGAC_s1v1_scaffolds.fa'
-
-        }
+            reference: 'file://../index/Chalara_fraxinea_TGAC_s1v1_scaffolds.fa' }
     },
-    {
-        chr: 'Cf746836_TGAC_s1v1_scaffold_12',
+    {   chr: 'Cf746836_TGAC_s1v1_scaffold_12',
         pos: '672',
         id: '.',
         ref: 'CAAA',
         alt: 'CAA',
         qual: '128',
         filter: '.',
-        varinfo: {
-            VAR: 'INDEL',
+        varinfo:
+        {   VAR: 'INDEL',
             IS: '16,0.727273',
             DP: '22',
             VDB: '1.896486e-01',
@@ -95,32 +88,26 @@ var validOutput = [
             AC1: '2',
             DP4: '0,0,0,20',
             MQ: '60',
-            FQ: '-94.5'
-        },
-        sampleinfo: {
-            'foxley_wood1_bwa-mem-sorted.bam': {
+            FQ: '-94.5' },
+        sampleinfo:
+            [ { NAME: 'foxley_wood1_bwa-mem-sorted.bam',
                 GT: '1/1',
                 PL: '169,60,0',
-                GQ: '99'
-            }
-        },
-        attributes: {
-            vcfver: 'VCFv4.1',
+                GQ: '99' } ],
+        attributes:
+        {   vcfver: 'VCFv4.1',
             samtools: '0.1.19-44428cd',
-            reference: 'file://../index/Chalara_fraxinea_TGAC_s1v1_scaffolds.fa'
-
-        }
+            reference: 'file://../index/Chalara_fraxinea_TGAC_s1v1_scaffolds.fa' }
     },
-    {
-        chr: 'Cf746836_TGAC_s1v1_scaffold_12',
+    {   chr: 'Cf746836_TGAC_s1v1_scaffold_12',
         pos: '2911',
         id: '.',
         ref: 'ATA',
         alt: 'ATACTCGGTA',
         qual: '214',
         filter: '.',
-        varinfo: {
-          VAR: 'INDEL',
+        varinfo:
+        {   VAR: 'INDEL',
             IS: '16,0.727273',
             DP: '22',
             VDB: '3.802706e-02',
@@ -128,21 +115,16 @@ var validOutput = [
             AC1: '2',
             DP4: '0,0,9,8',
             MQ: '60',
-            FQ: '-85.5'
-        },
-        sampleinfo: {
-            'foxley_wood1_bwa-mem-sorted.bam': {
+            FQ: '-85.5' },
+        sampleinfo:
+            [ { NAME: 'foxley_wood1_bwa-mem-sorted.bam',
                 GT: '1/1',
                 PL: '255,51,0',
-                GQ: '99'
-            }
-        },
-        attributes: {
-            vcfver: 'VCFv4.1',
+                GQ: '99' } ],
+        attributes:
+        {   vcfver: 'VCFv4.1',
             samtools: '0.1.19-44428cd',
-            reference: 'file://../index/Chalara_fraxinea_TGAC_s1v1_scaffolds.fa'
-
-        }
+            reference: 'file://../index/Chalara_fraxinea_TGAC_s1v1_scaffolds.fa' }
     }
 ];
 
